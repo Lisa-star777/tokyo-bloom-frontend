9df63ebf68e6f2a7ee560a1eda02039f89ceea4e
Lisa-star777 <aliskolota@mail.ru>
2026-05-14 16:15:13 +1000

    Update BouquetsView.vue

--- src/views/BouquetsView.vue
+++ src/views/BouquetsView.vue
@@ -282,7 +282,7 @@ export default {
     border-radius: 10px !important;
   }
   .product-card .product-image {
-    height: 100px !important;
+    height: 80px !important;
     border-radius: 6px !important;
   }
   .product-card .product-image img {
